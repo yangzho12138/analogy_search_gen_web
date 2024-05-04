4fce4a3af37e7808c2ddc303e15683ddcd39b6dd
Bhavya <bhavya2@illinois.edu>
2024-05-04 02:27:45 -0500

    Fix redis/celery concurrency bug

--- search/script.sh
+++ search/script.sh
@@ -1,3 +1,3 @@
-celery -A search worker --loglevel=info &
+celery -A search worker --loglevel=info --concurrency=3 -n searchworker1@%h &
 celery -A search beat --loglevel=info &
 python manage.py runserver 0.0.0.0:8001
