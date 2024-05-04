9271b4d1a87848edeca81026575aab8198e7ec73
Bhavya <bhavya2@illinois.edu>
2024-05-04 02:28:36 -0500

    Modify celery config

--- search/search/celery.py
+++ search/search/celery.py
@@ -8,7 +8,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search.settings')
 app = Celery('tasks',
              broker='amqps://vfkjnect:IrMSAhux-4FJP_d9S8z4X5UQWTTMgAqA@gull.rmq.cloudamqp.com/vfkjnect',
              backend='rpc://')
-
+app.conf.update(
+    task_serializer="pickle",
+    result_serializer="pickle",
+    accept_content=["pickle"]
+)
 app.conf.task_routes = {
     # 'auth.users.tasks.create_user': {'queue': 'user_created_queue'},
     'search.analogy.tasks.search_log': {'queue': 'search_log_queue'},
