ae48d9596ca96b7faa2dbbbea0bbfd8777b70899
Bhavya <bhavya2@illinois.edu>
2024-06-19 18:25:47 -0500

    adding missing import

--- web/pages/profile.js
+++ web/pages/profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Row, Col, Card, Form, Button } from 'react-bootstrap';
 import LogList from '../components/LogList';
 import axios from 'axios';
