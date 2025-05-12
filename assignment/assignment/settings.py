b4b0d9b6f058d0a92b25818171b32f4649bcdb88
yz96 <68858113+yangzho12138@users.noreply.github.com>
2025-05-11 23:24:24 -0700

    security issue

--- assignment/assignment/settings.py
+++ assignment/assignment/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-qd&*=0wkjcrq%*jxt#zvx7=+m$c&z1%%20u5+&rimnwcfg&i)j"
+# SECRET_KEY = "django-insecure-qd&*=0wkjcrq%*jxt#zvx7=+m$c&z1%%20u5+&rimnwcfg&i)j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
